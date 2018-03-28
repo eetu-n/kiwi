79b513ccd7bed11b3781e4ef507ef52ded86adc5
rargh <eetu.t.narhi@gmail.com>
2018-03-28 17:54:44 +0200

    All basic functionality implemented

--- kiwi.ino
+++ kiwi.ino
@@ -1,45 +1,76 @@
 #include <Arduino.h>
 
-#include <U8x8lib.h>     //Using u8g2 libraryE
-U8X8_SSD1306_128X64_NONAME_HW_I2C u8x8 (U8X8_PIN_NONE);
-
+#include <U8g2lib.h>     //Using u8g2 libraryE
 #include "OOCSI.h"
-const char* ssid = "Springerstraat 1";
-const char* password = "solutio@13579";
+#include <Wire.h>
+#include <SPI.h>
+#include <MFRC522.h>
+#include <FastLED.h>
+
+#define NUM_LEDS 8
+#define vibPin 12
+#define ledPin 13
+#define tagAmount 10
+
+const char* ssid = "Narhi";
+const char* password = "rusinapallo";
 const char* OOCSIName = "kiwi";
 const char* hostserver = "oocsi.id.tue.nl";
 OOCSI oocsi = OOCSI();
 
-#define vibPin 16
+int curMillis = 0;
 
-#include <SPI.h>
-#include <MFRC522.h>
-MFRC522 mfrc522(5, 17);
-
-int uidSize;
 String uid;
+String scannedUid;
+String uidList[tagAmount];
+int uidSize;
 String tempUid;
 String tempUid2;
 
-String word1 = "Message ";
-String word2 = " not set";
-
-String uidList[10];
-const char* messageList[10];
-int lineList[10];
-int clearLines;
-
+String messageList[tagAmount];
 String messageIn = "Hello World!";
 const char* messageOut;
+int messageTime = 1000;
+int messageTimeList[tagAmount];
 
-int delayer = 0;
-int timer = 0;
-bool vibrate = false;
-
+int lineList[tagAmount];
 int line = 0;
 
+int delayer = 0;
+int delayList[tagAmount];
+int timer = 0;
+int timeList[tagAmount];
+bool vibrate = false;
+bool vibList[tagAmount];
+int repeat = 1;
+int repeatList[tagAmount];
+int vibOn = false;
+int vibDelay;
+
+int hue = 0;
+int hueList[tagAmount];
+int brightness = 0;
+int curBrightness = 0;
+int targetBrightness = 0;
+int brightnessList[tagAmount];
+int tempLedMode = 0;
+int ledMode = 0;
+int ledModeList[tagAmount];
+int divBy = 10;
+bool direction = true;
+
+int ledMillis;
+int vibMillis;
+int nfcMillis;
+
+U8X8_SSD1306_128X64_ALT0_HW_I2C u8x8(/* reset=*/ U8X8_PIN_NONE);
+MFRC522 mfrc522(5, 17);
+CRGB leds[NUM_LEDS];
+
 void setup(void) {
-  Serial.begin(115200);
+  //Serial.begin(115200);
+  FastLED.addLeds<NEOPIXEL, ledPin>(leds, NUM_LEDS);
+  pinMode(vibPin, OUTPUT);
   mfrc522.PCD_SetAntennaGain(mfrc522.RxGain_max);
   SPI.begin();
   mfrc522.PCD_Init();
@@ -52,89 +83,230 @@ void setup(void) {
   u8x8.begin();
   u8x8.setPowerSave(0);
 
-  u8x8.setFont(u8x8_font_pxplusibmcgathin_f);
+  u8x8.setFont(u8x8_font_chroma48medium8_r);
+  u8x8.drawString(0, 0, "Welcome");
 
-  pinMode(vibPin, OUTPUT);
-  digitalWrite(vibPin, HIGH);
-  delay(500);
-  digitalWrite(vibPin, LOW);
+  ledMillis = millis();
+  vibMillis = millis();
+  nfcMillis = millis();
 
 }
 
 void loop(void) {
   if (mfrc522.PICC_IsNewCardPresent()) {
-    mfrc522.PICC_ReadCardSerial();
-
-    uidSize = mfrc522.uid.size;
-
-    for (int i = 0; i < uidSize; i++) {
-      if (i != 0) {
-        if (mfrc522.uid.uidByte[i] < 0x10) {
-          tempUid2 = " 0";
-        } else {
-          tempUid2 = " ";
-        }
-      }
-      tempUid = tempUid + tempUid2 + String(mfrc522.uid.uidByte[i], HEX);
-    }
-    Serial.println(tempUid);
-
-    uid = tempUid;
-    tempUid = "";
-
-    for (int i = 0; i < 10; i++) {
-      const int tempLine = lineList[i];
-      const char* tempMessage = messageList[i];
-      if (uidList[i] == uid) {
-        if (clearLines == 8) {
-          u8x8.clearDisplay();
-        }
-        u8x8.drawString(0, tempLine, tempMessage);
-        Serial.println(messageList[i]);
-        break;
-      }
-      if (uidList[i] == "") {
-        uidList[i] = uid;
-        messageList[i] = "";
-        break;
-      }
-    }
+    getUID();
+    updateLeds();
+    printText();
   }
+
+  ledPattern();
+  vibTimer();
   oocsi.check();
 }
 
 void processOOCSI() {
+  uid = oocsi.getString("uid", 0);
+  
   line = oocsi.getInt("line", 0);
   messageIn = oocsi.getString("message", "-200");
-  messageOut = messageIn.c_str();
+  
   delayer = oocsi.getInt("delay", 0);
   timer = oocsi.getInt("time", 0);
-  uid = oocsi.getString("uid", 0);
-  clearLines = oocsi.getInt("clearLines", 9);
+  repeat = oocsi.getInt("repeat", 0);
+  
+  hue = oocsi.getInt("hue", 0);
+  brightness = oocsi.getInt("brightness", 0);
+  tempLedMode = oocsi.getInt("ledMode", 0);
+  
 
-  for (int i = 0; i < 10; i++) {
+  for (int i = 0; i < tagAmount; i++) {
     if (uidList[i] == uid) {
-      messageList[i] = messageOut;
+      messageList[i] = messageIn;
       lineList[i] = line;
+      
+      repeatList[i] = repeat;
+      delayList[i] = delayer;
+      timeList[i] = timer;
+      
+      hueList[i] = hue;
+      brightnessList[i] = brightness;
+      ledModeList[i] = tempLedMode;
+
+      //Serial.print("Message: ");
+      //Serial.println(messageList[i]);
+      //Serial.print("Line: ");
+      //Serial.println(lineList[i]);
+      //Serial.print("Hue: ");
+      //Serial.println(hueList[i]);
+      //Serial.print("Brightness: ");
+      //Serial.println(brightnessList[i]);
+      //Serial.print("Led Mode: ");
+      //Serial.println(ledModeList[i]);
+      //Serial.print("Repeat: ");
+      //Serial.println(repeatList[i]);
       break;
-    }
-    if (uidList[i] == "") {
+    } else if (uidList[i] == "") {
       uidList[i] = uid;
-      messageList[i] = messageOut;
+      
+      messageList[i] = messageIn;
       lineList[i] = line;
+
+      repeatList[i] = repeat;
+      delayList[i] = delayer;
+      timeList[i] = timer;
+      
+      hueList[i] = hue;
+      brightnessList[i] = brightness;
+      ledModeList[i] = tempLedMode;
+
+      //Serial.print("UID: ");
+      //Serial.println(uidList[i]);
+      //Serial.print("Message: ");
+      //Serial.println(messageList[i]);
+      //Serial.print("Line: ");
+      //Serial.println(lineList[i]);
+      //Serial.print("Hue: ");
+      //Serial.println(hueList[i]);
+      //Serial.print("Brightness: ");
+      //Serial.println(brightnessList[i]);
+      //Serial.print("Led Mode: ");
+      //Serial.println(ledModeList[i]);
+      //Serial.print("Repeat: ");
+      //Serial.println(repeatList[i]);
       break;
     }
   }
-
-  Serial.print("Message for ");
-  Serial.print(uid);
-  Serial.print(" is ");
-  Serial.println(messageOut);
+  for (int i = 0; i < 4; i++) {
+    //Serial.println(messageList[i]);
+  }
 
   if (oocsi.getInt("vibrate", 0) == 0) {
     vibrate = false;
   } else {
     vibrate = true;
+  }
+}
+
+void updateLeds() {
+  for (int i = 0; i < tagAmount; i++) {
+    if (uidList[i] == scannedUid) {
+      const int tempHue = hueList[i];
+      const int tempBrightness = brightnessList[i];
+      targetBrightness = brightnessList[i];
+      curBrightness = brightnessList[i];
+      ledMode = ledModeList[i];
+      for (int i = 0; i < NUM_LEDS; i++) {
+        leds[i].setHue(tempHue);
+        leds[i].maximizeBrightness();
+        leds[i].nscale8(tempBrightness);
+      }
+    }
+  }
+  ledMillis = millis();
+  direction = true;
+  FastLED.show();
+}
+
+void getUID() {
+  mfrc522.PICC_ReadCardSerial();
+
+  uidSize = mfrc522.uid.size;
+
+  for (int i = 0; i < uidSize; i++) {
+    if (i != 0) {
+      if (mfrc522.uid.uidByte[i] < 0x10) {
+        tempUid2 = " 0";
+      } else {
+        tempUid2 = " ";
+      }
+    } else {
+      tempUid2 = "";
+    }
+    tempUid = tempUid + tempUid2 + String(mfrc522.uid.uidByte[i], HEX);
+  }
+  //Serial.println(tempUid);
+  scannedUid = tempUid;
+  tempUid = "";
+}
+
+void printText() {
+  for (int i = 0; i < tagAmount; i++) {
+    if (uidList[i] == scannedUid) {
+      messageOut = messageList[i].c_str();
+      u8x8.clearDisplay();
+      u8x8.drawString(0, lineList[i], messageOut);
+      break;
+    } else if (uidList[i] == "") {
+      uidList[i] = scannedUid;
+      messageList[i] = "";
+      break;
+    }
+  }
+}
+
+void ledPattern() {
+  if (ledMode == 0) {
+    return;
+  } else if (ledMode == 1) {
+    divBy = 10;
+  } else if (ledMode == 2) {
+    divBy = 20;
+  }
+
+  curMillis = millis();
+
+  if (curBrightness <= 1 || curBrightness >= targetBrightness) {
+    direction = !direction;
+  }
+
+  if (curMillis - ledMillis >= 10) {
+    if (direction) {
+      curBrightness = int(curBrightness - (targetBrightness / divBy));
+    } else {
+      curBrightness = int(curBrightness + (targetBrightness / divBy));
+    }
+
+    if (curBrightness < 1) {
+      curBrightness = 1;
+    } else if (curBrightness > targetBrightness) {
+      curBrightness = targetBrightness;
+    }
+
+    for (int i = 0; i < NUM_LEDS; i++) {
+      leds[i].maximizeBrightness();
+      leds[i].nscale8(curBrightness);
+    }
+
+    FastLED.show();
+    ledMillis = millis();
+  }
+}
+
+void vibTimer() {
+  curMillis = millis();
+  for (int i = 0; i < tagAmount; i++) {
+    if (uidList[i] == scannedUid) {
+      if (repeatList[i] > 0) {
+        if (vibOn) {
+          vibDelay = timeList[i];
+        } else {
+          vibDelay = delayList[i];
+        }
+        if (curMillis - vibMillis >= vibDelay) {
+          repeatList[i]--;
+          if (vibOn) {
+            //Serial.println("Low");
+            digitalWrite(vibPin, LOW);
+            vibOn = false;
+          } else {
+            //Serial.println("High");
+            digitalWrite(vibPin, HIGH);
+            vibOn = true;
+          }
+          vibMillis = millis();
+        }
+      }
+    }
   }
 }
 
